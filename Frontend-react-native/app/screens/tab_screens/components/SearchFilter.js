58983f0067994dfa754ee433f896171ec86939e4
unknown <lnatekin@asu.edu>
2024-01-09 20:41:15 -0700

    minor changes

--- Frontend-react-native/app/screens/tab_screens/components/SearchFilter.js
+++ Frontend-react-native/app/screens/tab_screens/components/SearchFilter.js
@@ -3,10 +3,11 @@ import { View, Text } from "react-native";
 import UserCard from "./UserCard";
 
 export default SearchFilter = ({ input, data, setInput }) => {
+  console.log(data.data);
   return (
     <View style={{ flex: 1 }}>
       <FlashList
-        data={data}
+        data={data.data}
         renderItem={({ item }) => {
           if (item.name.toLowerCase().includes(input.toLowerCase())) {
             return <UserCard item={item} />;
