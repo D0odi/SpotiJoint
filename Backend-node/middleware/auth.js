d66c43e90e90494134878c93c5a3a475a3a88c44
Lev Natekin <natekinlev@gmail.com>
2024-01-26 02:41:48 -0700

    Backend: #5

--- Backend-node/middleware/auth.js
+++ Backend-node/middleware/auth.js
@@ -3,38 +3,33 @@ const User = require("../models/user");
 
 exports.isAuth = async (req, res, next) => {
   console.log(req.headers);
-  if (req.headers && req.headers.auth) {
-    console.log(req.headers.auth);
-    const token = req.headers.auth.split(" ")[1];
-    try {
-      const decode = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await User.findById(decode.userId);
+  console.log(req.headers.auth);
+  const token = req.headers.auth.split(" ")[1];
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decode.userId);
 
-      if (!user) {
-        res.json({ success: false, message: "Unauthorized access" });
-      }
-
-      req.user = user;
-      next();
-    } catch (error) {
-      if (error.name === "JsonWebTokenError") {
-        res.json({
-          success: false,
-          message: "Unauthorized access",
-          error: error.message,
-        });
-      }
-      if (error.name === "TokenExpiredError") {
-        res.json({
-          success: false,
-          message: "Token expired, try to login again",
-        });
-      }
-
-      res.json({ success: false, message: "Unauthorized access 3" });
+    if (!user) {
+      res.json({ success: false, message: "Unauthorized access" });
     }
-  } else {
-    console.log("Unauthorized access, no headers?");
-    res.json({ success: false, message: "Unauthorized access 4" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      res.json({
+        success: false,
+        message: "Unauthorized access",
+        error: error.message,
+      });
+    }
+    if (error.name === "TokenExpiredError") {
+      res.json({
+        success: false,
+        message: "Token expired, try to login again",
+      });
+    }
+
+    res.json({ success: false, message: "Unauthorized access 3" });
   }
 };
